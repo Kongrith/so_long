73fccd39d66ec185de4b220b5c716555d8502fb2
kongrith <capuchino_cartoon@hotmail.com>
2024-12-02 15:16:25 +0700

    add enclose func

--- check_map.c
+++ check_map.c
@@ -6,13 +6,13 @@
 /*   By: toon <toon@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/02 06:11:17 by khkomasa          #+#    #+#             */
-/*   Updated: 2024/12/02 13:14:34 by toon             ###   ########.fr       */
+/*   Updated: 2024/12/02 15:14:52 by toon             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	floodfill(t_data *data, char **gridmap, t_point cur)
+void floodfill(t_data *data, char **gridmap, t_point cur, char to_fill)
 {
 	if (gridmap[cur.y][cur.x] == 'C')
 	{
@@ -24,49 +24,70 @@ void	floodfill(t_data *data, char **gridmap, t_point cur)
 		data->count_e += 1;
 		gridmap[cur.y][cur.x] = '0';
 	}
-	if (cur.y < 0 || cur.y >= data->row || cur.x < 0 || cur.x >= data->col || \
-		gridmap[cur.y][cur.x] != '0')
+	if (cur.y < 0 || cur.y >= data->row || cur.x < 0 || cur.x >= data->col ||
+		gridmap[cur.y][cur.x] != to_fill)
 		return ;
 	gridmap[cur.y][cur.x] = 'F';
-	floodfill(data, gridmap, (t_point){cur.x - 1, cur.y});
-	floodfill(data, gridmap, (t_point){cur.x + 1, cur.y});
-	floodfill(data, gridmap, (t_point){cur.x, cur.y - 1});
-	floodfill(data, gridmap, (t_point){cur.x, cur.y + 1});
+	floodfill(data, gridmap, (t_point){cur.x - 1, cur.y}, to_fill);
+	floodfill(data, gridmap, (t_point){cur.x + 1, cur.y}, to_fill);
+	floodfill(data, gridmap, (t_point){cur.x, cur.y - 1}, to_fill);
+	floodfill(data, gridmap, (t_point){cur.x, cur.y + 1}, to_fill);
 }
 
-void	check_wall(t_data *data)
+void check_wall(t_data *data, char **gridmap)
 {
-	(void)data;
+	int i;
+	int j;
+	int chk_enclose;
+
+	i = 0;
+	j = -1;
+	chk_enclose = 1;
+	while (++j < data->col && chk_enclose)
+		if (gridmap[i][j] != '1')
+			chk_enclose = 0;
+	j--;
+	while (++i < data->row && chk_enclose)
+		if (gridmap[i][j] != '1')
+			chk_enclose = 0;
+	i--;
+	while (--j >= 0 && chk_enclose)
+		if (gridmap[i][j] != '1')
+			chk_enclose = 0;
+	j++;
+	while (--i >= 0 && chk_enclose)
+		if (gridmap[i][j] != '1')
+			chk_enclose = 0;
+	if (chk_enclose == 0)
+		err_handler(data, "Err: Map don't enclose wall completely!!\n", 0);
 }
 
-void	check_valid_path(t_data *data, int is_found, int i, int j)
+void	check_valid_path(t_data *data, char **gridmap, int is_found, \
+t_point index)
 {
 	t_point	begin;
-	char	**gridmap;
 
-	while (i < data->row && is_found == 0)
+	while (index.y < data->row && is_found == 0)
 	{
-		j = 0;
-		while (j < data->col && is_found == 0)
+		index.x = 0;
+		while (index.x < data->col && is_found == 0)
 		{
-			if (data->map[i][j] == 'P')
+			if (data->map[index.y][index.x] == 'P')
 				is_found = 1;
 			else
-				j++;
+				index.x++;
 		}
 		if (is_found != 1)
-			i++;
+			index.y++;
 	}
-	begin.x = j;
-	begin.y = i;
-	gridmap = make_gridmap(data->map, data->row, data->col);
-	gridmap[i][j] = '0';
-	floodfill(data, gridmap, begin);
+	begin.x = index.x;
+	begin.y = index.y;
+	gridmap[index.y][index.x] = '0';
+	floodfill(data, gridmap, begin, '0');
 	if (data->count_e != data->exit)
 		err_handler(data, "Err: Player can not reach exit !!\n", 0);
 	if (data->count_c != data->collect)
 		err_handler(data, "Err: Player can not reach all collectables !!\n", 0);
-	free_double_ptr(gridmap);
 }
 
 void	check_size(t_data *data)
@@ -79,14 +100,19 @@ void	check_size(t_data *data)
 
 void	check_map(t_data *data)
 {
-	int		i;
-	int		j;
 	int		is_found;
+	char	**gridmap;
+	t_point	index;
 
-	i = 0;
-	j = 0;
+	index.x = 0;
+	index.y = 0;
 	is_found = 0;
 	check_size(data);
-	check_wall(data);
-	check_valid_path(data, is_found, i, j);
+	gridmap = make_gridmap(data->map, data->row, data->col);
+	ft_printf("before check_wall\n");
+	check_wall(data, gridmap);
+	ft_printf("after check_wall\n");
+	check_valid_path(data, gridmap, is_found, index);
+	ft_printf("after check_valid_path\n");
+	free_double_ptr(gridmap);
 }
